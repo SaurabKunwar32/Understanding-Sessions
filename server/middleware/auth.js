29049f45874d5d816ac3691449f921b6997c3d15
Saurab Kunwar <saurabkunwar188@gmail.com>
2025-08-12 09:21:15 +0545

    middleware

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,21 +2,21 @@ import Session from "../models/Session.js";
 
 
 export const checkSession = async (req, res, next) => {
-  // const { sid } = req.signedCookies;
-  // console.log(sid);
+  const { sid } = req.signedCookies;
+  console.log(sid);
 
-  // if (!sid) return res.status(401).json({ message: "Session ID not found" });
+  if (!sid) return res.status(401).json({ message: "Session ID not found" });
 
-  // try {
-  //   const userSession = await Session.findById(sid);
+  try {
+    const userSession = await Session.findById(sid);
 
-  //   if (!userSession) {
-  //     res.clearCookie("sid");
-  //     return res.status(401).json({ message: "Session Expired" });
-  //   }
-  //   req.userSession = userSession;
-  //   next();
-  // } catch (error) {
-  //   return res.status(500).json({ message: "Server error in session", error });
-  // }
+    if (!userSession) {
+      res.clearCookie("sid");
+      return res.status(401).json({ message: "Session Expired" });
+    }
+    req.userSession = userSession;
+    next();
+  } catch (error) {
+    return res.status(500).json({ message: "Server error in session", error });
+  }
 };
